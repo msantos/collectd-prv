c8eaf72223097c7948a5dda8a85021bee6bd8b94
Michael Santos <michael.santos@gmail.com>
2017-12-02 10:19:46 -0500

    capsicum: return value of setrlimit()

--- sandbox_capsicum.c
+++ sandbox_capsicum.c
@@ -27,10 +27,7 @@ prv_sandbox_init()
 {
     struct rlimit rl = {0};
 
-    if (setrlimit(RLIMIT_NPROC, &rl) < 0)
-        return -1;
-
-    return 0;
+    return setrlimit(RLIMIT_NPROC, &rl);
 }
 
     int
