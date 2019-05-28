29573a3a29a76eb1698b37ce0bbdf713bb5a6f60
Michael Santos <michael.santos@gmail.com>
2019-05-28 07:56:07 -0400

    seccomp: reduce allowed syscalls
    Allowed syscalls were based on pledge(2). Restrict to minimum required
    syscalls.

--- sandbox_seccomp.c
+++ sandbox_seccomp.c
@@ -101,21 +101,11 @@ int prv_sandbox_init() {
 #ifdef __NR_close
       SC_ALLOW(close),
 #endif
-#ifdef __NR_dup
-      SC_ALLOW(dup),
-#endif
-#ifdef __NR_dup2
-      SC_ALLOW(dup2),
-#endif
-#ifdef __NR_dup3
-      SC_ALLOW(dup3),
-#endif
+
 #ifdef __NR_exit_group
       SC_ALLOW(exit_group),
 #endif
-#ifdef __NR_fchdir
-      SC_ALLOW(fchdir),
-#endif
+
 #ifdef __NR_fcntl
       SC_ALLOW(fcntl),
 #endif
@@ -128,129 +118,11 @@ int prv_sandbox_init() {
 #ifdef __NR_fstat64
       SC_ALLOW(fstat64),
 #endif
-#ifdef __NR_fsync
-      SC_ALLOW(fsync),
-#endif
-#ifdef __NR_ftruncate
-      SC_ALLOW(ftruncate),
-#endif
-#ifdef __NR_getdents
-      SC_ALLOW(getdents),
-#endif
-#ifdef __NR_getdtablecount
-      SC_ALLOW(getdtablecount),
-#endif
-#ifdef __NR_getegid
-      SC_ALLOW(getegid),
-#endif
-#ifdef __NR_getentropy
-      SC_ALLOW(getentropy),
-#endif
-#ifdef __NR_geteuid
-      SC_ALLOW(geteuid),
-#endif
-#ifdef __NR_getgid
-      SC_ALLOW(getgid),
-#endif
-#ifdef __NR_getgroups
-      SC_ALLOW(getgroups),
-#endif
-#ifdef __NR_getitimer
-      SC_ALLOW(getitimer),
-#endif
-#ifdef __NR_getlogin
-      SC_ALLOW(getlogin),
-#endif
-#ifdef __NR_getpgid
-      SC_ALLOW(getpgid),
-#endif
-#ifdef __NR_getpgrp
-      SC_ALLOW(getpgrp),
-#endif
-#ifdef __NR_getpid
-      SC_ALLOW(getpid),
-#endif
-#ifdef __NR_getppid
-      SC_ALLOW(getppid),
-#endif
-#ifdef __NR_getresgid
-      SC_ALLOW(getresgid),
-#endif
-#ifdef __NR_getresuid
-      SC_ALLOW(getresuid),
-#endif
-#ifdef __NR_getrlimit
-      SC_ALLOW(getrlimit),
-#endif
-#ifdef __NR_getsid
-      SC_ALLOW(getsid),
-#endif
-#ifdef __NR_getthrid
-      SC_ALLOW(getthrid),
-#endif
+
 #ifdef __NR_gettimeofday
       SC_ALLOW(gettimeofday),
 #endif
-#ifdef __NR_getuid
-      SC_ALLOW(getuid),
-#endif
-#ifdef __NR_getuid
-      SC_ALLOW(getuid),
-#endif
-#ifdef __NR_ioctl
-      SC_ALLOW(ioctl),
-#endif
-#ifdef __NR_issetugid
-      SC_ALLOW(issetugid),
-#endif
-#ifdef __NR_lseek
-      SC_ALLOW(lseek),
-#endif
-#ifdef __NR__llseek
-      SC_ALLOW(_llseek),
-#endif
-#ifdef __NR_lstat
-      SC_ALLOW(lstat),
-#endif
-#ifdef __NR_lstat64
-      SC_ALLOW(lstat64),
-#endif
-#ifdef __NR_madvise
-      SC_ALLOW(madvise),
-#endif
-#ifdef __NR_minherit
-      SC_ALLOW(minherit),
-#endif
-#ifdef __NR_mmap
-      SC_ALLOW(mmap),
-#endif
-#ifdef __NR_mmap2
-      SC_ALLOW(mmap2),
-#endif
-#ifdef __NR_mprotect
-      SC_ALLOW(mprotect),
-#endif
-#ifdef __NR_mquery
-      SC_ALLOW(mquery),
-#endif
-#ifdef __NR_mremap
-      SC_ALLOW(mremap),
-#endif
-#ifdef __NR_munmap
-      SC_ALLOW(munmap),
-#endif
-#ifdef __NR_nanosleep
-      SC_ALLOW(nanosleep),
-#endif
-#ifdef __NR_pipe
-      SC_ALLOW(pipe),
-#endif
-#ifdef __NR_pipe2
-      SC_ALLOW(pipe2),
-#endif
-#ifdef __NR_poll
-      SC_ALLOW(poll),
-#endif
+
 #ifdef __NR_pread
       SC_ALLOW(pread),
 #endif
@@ -269,27 +141,7 @@ int prv_sandbox_init() {
 #ifdef __NR_readv
       SC_ALLOW(readv),
 #endif
-#ifdef __NR_recvfrom
-      SC_ALLOW(recvfrom),
-#endif
-#ifdef __NR_recvmsg
-      SC_ALLOW(recvmsg),
-#endif
-#ifdef __NR_select
-      SC_ALLOW(select),
-#endif
-#ifdef __NR_sendmsg
-      SC_ALLOW(sendmsg),
-#endif
-#ifdef __NR_sendto
-      SC_ALLOW(sendto),
-#endif
-#ifdef __NR_setitimer
-      SC_ALLOW(setitimer),
-#endif
-#ifdef __NR_shutdown
-      SC_ALLOW(shutdown),
-#endif
+
 #ifdef __NR_sigaction
       SC_ALLOW(sigaction),
 #endif
@@ -299,17 +151,9 @@ int prv_sandbox_init() {
 #ifdef __NR_sigreturn
       SC_ALLOW(sigreturn),
 #endif
-#ifdef __NR_socketpair
-      SC_ALLOW(socketpair),
-#endif
-#ifdef __NR_umask
-      SC_ALLOW(umask),
-#endif
+
 #ifdef __NR_uname
       SC_ALLOW(uname),
-#endif
-#ifdef __NR_wait4
-      SC_ALLOW(wait4),
 #endif
 #ifdef __NR_write
       SC_ALLOW(write),
@@ -317,57 +161,7 @@ int prv_sandbox_init() {
 #ifdef __NR_writev
       SC_ALLOW(writev),
 #endif
-#ifdef __NR_chdir
-      SC_ALLOW(chdir),
-#endif
-#ifdef __NR_getcwd
-      SC_ALLOW(getcwd),
-#endif
-#ifdef __NR_openat
-      SC_ALLOW(openat),
-#endif
-#ifdef __NR_open
-      SC_ALLOW(open),
-#endif
-#ifdef __NR_fstatat
-      SC_ALLOW(fstatat),
-#endif
-#ifdef __NR_faccessat
-      SC_ALLOW(faccessat),
-#endif
-#ifdef __NR_readlinkat
-      SC_ALLOW(readlinkat),
-#endif
-#ifdef __NR_lstat
-      SC_ALLOW(lstat),
-#endif
-#ifdef __NR_chmod
-      SC_ALLOW(chmod),
-#endif
-#ifdef __NR_fchmod
-      SC_ALLOW(fchmod),
-#endif
-#ifdef __NR_fchmodat
-      SC_ALLOW(fchmodat),
-#endif
-#ifdef __NR_chflags
-      SC_ALLOW(chflags),
-#endif
-#ifdef __NR_chflagsat
-      SC_ALLOW(chflagsat),
-#endif
-#ifdef __NR_chown
-      SC_ALLOW(chown),
-#endif
-#ifdef __NR_fchown
-      SC_ALLOW(fchown),
-#endif
-#ifdef __NR_fchownat
-      SC_ALLOW(fchownat),
-#endif
-#ifdef __NR_getfsstat
-      SC_ALLOW(getfsstat),
-#endif
+
 #ifdef __NR_restart_syscall
       SC_ALLOW(restart_syscall),
 #endif
@@ -406,12 +200,6 @@ int prv_sandbox_stdin() {
 /* Syscalls to allow */
 
 /* Used to test if input is file or stdin */
-#ifdef __NR_fcntl
-      SC_ALLOW(fcntl),
-#endif
-#ifdef __NR_fcntl64
-      SC_ALLOW(fcntl64),
-#endif
 #ifdef __NR_fstat
       SC_ALLOW(fstat),
 #endif
@@ -428,138 +216,15 @@ int prv_sandbox_stdin() {
 #ifdef __NR_close
       SC_ALLOW(close),
 #endif
-#ifdef __NR_dup
-      SC_ALLOW(dup),
-#endif
-#ifdef __NR_dup2
-      SC_ALLOW(dup2),
-#endif
-#ifdef __NR_dup3
-      SC_ALLOW(dup3),
-#endif
+
 #ifdef __NR_exit_group
       SC_ALLOW(exit_group),
 #endif
-#ifdef __NR_fchdir
-      SC_ALLOW(fchdir),
-#endif
-#ifdef __NR_fsync
-      SC_ALLOW(fsync),
-#endif
-#ifdef __NR_ftruncate
-      SC_ALLOW(ftruncate),
-#endif
-#ifdef __NR_getdents
-      SC_ALLOW(getdents),
-#endif
-#ifdef __NR_getdtablecount
-      SC_ALLOW(getdtablecount),
-#endif
-#ifdef __NR_getegid
-      SC_ALLOW(getegid),
-#endif
-#ifdef __NR_getentropy
-      SC_ALLOW(getentropy),
-#endif
-#ifdef __NR_geteuid
-      SC_ALLOW(geteuid),
-#endif
-#ifdef __NR_getgid
-      SC_ALLOW(getgid),
-#endif
-#ifdef __NR_getgroups
-      SC_ALLOW(getgroups),
-#endif
-#ifdef __NR_getitimer
-      SC_ALLOW(getitimer),
-#endif
-#ifdef __NR_getlogin
-      SC_ALLOW(getlogin),
-#endif
-#ifdef __NR_getpgid
-      SC_ALLOW(getpgid),
-#endif
-#ifdef __NR_getpgrp
-      SC_ALLOW(getpgrp),
-#endif
-#ifdef __NR_getpid
-      SC_ALLOW(getpid),
-#endif
-#ifdef __NR_getppid
-      SC_ALLOW(getppid),
-#endif
-#ifdef __NR_getresgid
-      SC_ALLOW(getresgid),
-#endif
-#ifdef __NR_getresuid
-      SC_ALLOW(getresuid),
-#endif
-#ifdef __NR_getrlimit
-      SC_ALLOW(getrlimit),
-#endif
-#ifdef __NR_getsid
-      SC_ALLOW(getsid),
-#endif
-#ifdef __NR_getthrid
-      SC_ALLOW(getthrid),
-#endif
+
 #ifdef __NR_gettimeofday
       SC_ALLOW(gettimeofday),
 #endif
-#ifdef __NR_getuid
-      SC_ALLOW(getuid),
-#endif
-#ifdef __NR_getuid
-      SC_ALLOW(getuid),
-#endif
-#ifdef __NR_ioctl
-      SC_ALLOW(ioctl),
-#endif
-#ifdef __NR_issetugid
-      SC_ALLOW(issetugid),
-#endif
-#ifdef __NR_lseek
-      SC_ALLOW(lseek),
-#endif
-#ifdef __NR__llseek
-      SC_ALLOW(_llseek),
-#endif
-#ifdef __NR_madvise
-      SC_ALLOW(madvise),
-#endif
-#ifdef __NR_minherit
-      SC_ALLOW(minherit),
-#endif
-#ifdef __NR_mmap
-      SC_ALLOW(mmap),
-#endif
-#ifdef __NR_mmap2
-      SC_ALLOW(mmap2),
-#endif
-#ifdef __NR_mprotect
-      SC_ALLOW(mprotect),
-#endif
-#ifdef __NR_mquery
-      SC_ALLOW(mquery),
-#endif
-#ifdef __NR_mremap
-      SC_ALLOW(mremap),
-#endif
-#ifdef __NR_munmap
-      SC_ALLOW(munmap),
-#endif
-#ifdef __NR_nanosleep
-      SC_ALLOW(nanosleep),
-#endif
-#ifdef __NR_pipe
-      SC_ALLOW(pipe),
-#endif
-#ifdef __NR_pipe2
-      SC_ALLOW(pipe2),
-#endif
-#ifdef __NR_poll
-      SC_ALLOW(poll),
-#endif
+
 #ifdef __NR_pread
       SC_ALLOW(pread),
 #endif
@@ -578,27 +243,7 @@ int prv_sandbox_stdin() {
 #ifdef __NR_readv
       SC_ALLOW(readv),
 #endif
-#ifdef __NR_recvfrom
-      SC_ALLOW(recvfrom),
-#endif
-#ifdef __NR_recvmsg
-      SC_ALLOW(recvmsg),
-#endif
-#ifdef __NR_select
-      SC_ALLOW(select),
-#endif
-#ifdef __NR_sendmsg
-      SC_ALLOW(sendmsg),
-#endif
-#ifdef __NR_sendto
-      SC_ALLOW(sendto),
-#endif
-#ifdef __NR_setitimer
-      SC_ALLOW(setitimer),
-#endif
-#ifdef __NR_shutdown
-      SC_ALLOW(shutdown),
-#endif
+
 #ifdef __NR_sigaction
       SC_ALLOW(sigaction),
 #endif
@@ -607,15 +252,6 @@ int prv_sandbox_stdin() {
 #endif
 #ifdef __NR_sigreturn
       SC_ALLOW(sigreturn),
-#endif
-#ifdef __NR_socketpair
-      SC_ALLOW(socketpair),
-#endif
-#ifdef __NR_umask
-      SC_ALLOW(umask),
-#endif
-#ifdef __NR_wait4
-      SC_ALLOW(wait4),
 #endif
 #ifdef __NR_write
       SC_ALLOW(write),
