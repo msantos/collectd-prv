a5cc6c4f99a2d035c59c7fca2411ba828f3807db
Michael Santos <michael.santos@gmail.com>
2023-03-30 06:17:03 -0400

    rlimit: disable fs writes
    Use RLIMIT_FSIZE to disable writes to the filesystem when stdout is not
    a regular file.

--- restrict_process_rlimit.c
+++ restrict_process_rlimit.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2017-2023, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,7 +15,10 @@
 #include "restrict_process.h"
 #ifdef RESTRICT_PROCESS_rlimit
 #include <sys/resource.h>
+#include <sys/stat.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int restrict_process_init() {
   struct rlimit rl_zero = {0};
@@ -25,10 +28,16 @@ int restrict_process_init() {
 
 int restrict_process_stdin() {
   struct rlimit rl_zero = {0};
+  struct stat sb = {0};
 
-  if (setrlimit(RLIMIT_NOFILE, &rl_zero) < 0)
+  if (fstat(STDOUT_FILENO, &sb) < 0)
     return -1;
 
-  return setrlimit(RLIMIT_FSIZE, &rl_zero);
+  if (!S_ISREG(sb.st_mode)) {
+    if (setrlimit(RLIMIT_FSIZE, &rl_zero) < 0)
+      return -1;
+  }
+
+  return setrlimit(RLIMIT_NOFILE, &rl_zero);
 }
 #endif
